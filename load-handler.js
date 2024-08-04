ece99a8c08d9bff325af4c4dc8f72a0a983597f0
sillyyash <yashranaforyou@gmail.com>
2024-08-04 23:23:57 +0530

    w

--- load-handler.js
+++
@@ -1,8 +0,0 @@
-console.log("JavaScript file loaded");
-window.onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
-  
